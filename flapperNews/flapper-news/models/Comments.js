dea325333a0941aebd420f6bc049b318b6088bee
Udaiveer Singh <uda4000@hotmail.com>
2015-08-17 01:09:15 -0700

    finished backend api/testing/backups

--- flapperNews/flapper-news/models/Comments.js
+++ flapperNews/flapper-news/models/Comments.js
@@ -4,8 +4,13 @@ var CommentSchema = new mongoose.Schema({
     body: String,
     author: String,
     upvotes: {type: Number, default: 0},
-    comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post' }
 });
+
+CommentSchema.methods.upvote = function(cb) {
+    this.upvotes += 1; 
+    this.save(cb); 
+}; 
 
 mongoose.model('Comment', CommentSchema);
 
--- flapperNews/flapper-news/routes/index.js
+++ flapperNews/flapper-news/routes/index.js
@@ -29,7 +29,7 @@ router.get('/', function(req, res, next) {
  * */
 router.get('/partials/:name', function(req, res, next) {
     var name = req.params.name; 
-    var filePath = path.join('partials/', name+'.html');
+    var filePath = path.join('partials/', name + '.html');
     res.sendFile(filePath, options, function(err) {
         if(err) {
             res.status(err.status).end();
@@ -43,7 +43,10 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+var myModels = {'comment': Comment, 'post': Post}; 
 
+
+/** return all posts in json */ 
 router.get('/posts', function (req, res, next) {
     Post.find(function(err, posts) {
         if(err) { return next(err); }
@@ -52,6 +55,7 @@ router.get('/posts', function (req, res, next) {
     });
 });
 
+/** add a post to database */ 
 router.post('/posts', function(req, res, next) {
     var post = new Post(req.body);
 
@@ -62,23 +66,109 @@ router.post('/posts', function(req, res, next) {
     });
 });
 
+/**
+* This function pasres a url with :post and assigns 
+* a post property to req obj, :post is a variable 
+* for the post id 
+*/
 router.param('post', function(req, res, next, id) {
    var query = Post.findById(id);
-
    query.exec(function(err, post) { 
        if(err) { return next(err); }
        if(!post) { return next(new Error('can\'t find post')); }
-
        req.post = post;
-       return next(); 
+       next(); 
    });
 });
 
+/**
+* Delete a post by id and all related comments
+*/
 router.delete('/posts/:post', function(req, res, next) {
-    Post.findOneAndRemove(req.post.id, function(err, doc, result) {
+    Post.findByIdAndRemove(req.post.id, function(err, doc) {
         if(err) { console.log("err in delete"); return next(new Error('can\'t delete post')); }
+        
+        console.log(doc);
+        
+        var i = doc.comments.length; 
+        console.log(doc.comments.length);
+        /*jshint -W083 */
+        while(i--) { 
+            Comment.findByIdAndRemove(doc.comments[i], function(err, result) {
+                console.log(result);
+                if(err) { /** handle **/ }
+            });
+        }
 
         res.json(doc);
-    });   // executes
+    });  
 });
+
+/**
+* increment upvote of post by 1
+*/
+router.put('/posts/:post/upvotes', function(req, res, next) {
+    req.post.upvote(function(err, post) { 
+        if(err) { return next(err); } 
+
+        res.json(post); 
+    });
+});
+
+/**
+* and a comment to a post object  
+*/
+router.post('/posts/:post/comments', function(req, res, next) {
+    console.log(req.body);
+    var comment = new Comment(req.body);
+    comment.post = req.post; 
+
+    comment.save(function(err, comment) {
+        if(err) { return next(err); }
+
+        req.post.comments.push(comment); 
+        req.post.save(function(err, post) {
+            if(err) { return next(err); }
+
+        res.json(comment); 
+        });
+    });
+});
+
+/**
+* get a post and all the realted comments
+*/
+router.get('/posts/:post', function(req, res, next) {
+    req.post.populate('comments', function(err, post) {
+        if(err) { return next(err); } 
+
+        res.json(post); 
+    });
+});
+
+/**
+* parses the query string to assign req the comment object 
+* property
+*/
+router.param('comment', function(req, res, next, id) {
+   Comment.findById(id, function(err, comment) {
+       if(err) { next(err); } 
+       if(!comment) { next(new Error('can\'t find post')); }
+       
+       req.comment = comment;
+       next(); 
+   });
+});
+
+/**
+* uses :post and :upvote to upvote a comment 
+*/
+router.put('/posts/:post/comments/:comment/upvotes', function(req, res, next) {
+    req.comment.upvote(function(err, comm) {
+        if(err) { next(err); } 
+        
+        res.json(comm);
+    });
+});
+
 module.exports = router;
