1008a4f544be1a6b12c0669bb969b931f9c6b0a1
Udaiveer Singh <uda4000@hotmail.com>
2015-08-19 21:44:35 -0700

    simple login w/o authentication

--- JWT-demo/app.js
+++ JWT-demo/app.js
@@ -1,17 +1,36 @@
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
-
+var bodyParser = require('body-parser'); 
+var jwt = require('jsonwebtoken');
 
 var app = express();
+var user = {username: 'admin' , password: 'password'}; 
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/random-user', function(req, res) {
     var user = faker.helpers.createCard();
     user.avatar = faker.image.avatar();
     res.json(user);
 });
+
+app.post('/login', authenticate, function(req, res, next) {
+    res.json(user);
+});
+
+
+function authenticate(req, res, next) {
+    var body = req.body;
+    console.log(body); 
+    console.log(user);
+    if(body.username === user.username && body.password === user.password) {
+        next();
+    } else {
+        res.status(400).end('invalid username or password'); 
+    }
+}
 
 app.listen(3000, function() {
     console.log('App listening on localhost:3000');
--- JWT-demo/public/app.js
+++ JWT-demo/public/app.js
@@ -1,19 +1,31 @@
 (function() {
-   var app = angular.module('app', []);
+    var app = angular.module('app', []);
 
-   app.constant('API_URL', 'http://localhost:3000');
+    app.constant('API_URL', 'http://localhost:3000');
 
-   app.controller('MainCtrl', function(RandomUserFactory) {
+    app.controller('MainCtrl', function(RandomUserFactory, UserFactory) {
        var vm = this; 
 
        vm.getRandomUser = getRandomUser;
+       vm.login = login; 
 
-       function getRandomUser() {
-           RandomUserFactory.getUser().then(function(response) {
-               vm.randomUser = response.data;
-            });
-       }
-   });
+    function getRandomUser() {
+        RandomUserFactory.getUser().then(function(response) {
+           vm.randomUser = response.data;
+        });
+    }
+
+    function login(username, password) {
+        UserFactory.login(username, password).then(function(response) { 
+            vm.user = response.data;
+        }, handleError); 
+    }
+       
+    function handleError(response) {
+        alert('Error: ' + response.data);
+    }
+
+});
 
    app.factory('RandomUserFactory', function($http, API_URL) {
        return {
@@ -22,6 +34,19 @@
 
        function getUser() {
            return $http.get(API_URL + '/random-user');
+       }
+   });
+
+   app.factory('UserFactory', function UserFacatory($http, API_URL) {
+       return {
+           login: login
+       };
+
+       function login(username, password) {
+           return $http.post(API_URL + '/login', { 
+               username: username,
+               password: password
+           });
        }
    });
 
