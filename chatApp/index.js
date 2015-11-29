8f2b422c4cdc790339389eebe119a495880f4fe3
Udaiveer <uda4000@hotmail.com>
2015-11-29 13:47:35 -0800

    static web routing done and tested

--- chatApp/index.js
+++ chatApp/index.js
@@ -4,10 +4,11 @@ var mongo = require('mongodb').MongoClient;
 var http = require('http'); 
 var app = http.createServer(router);
 var client = require('socket.io').listen(app).sockets;
-app.listen(8080, () => { console.log('listening to port 8080'); });
 var url = require('url'); 
+var fs = require('fs');
+var path = require('path');
 
-
+app.listen(8080, () => { console.log('listening to port 8080'); });
 //routing stuff 
 var routes = {
     'GET': {
@@ -35,19 +36,36 @@ var routes = {
     }
 };
 
-//do all website routing
+var mimes = {
+    '.html' :   'text/html',
+    '.css'  :   'text/css',
+    '.js'   :   'text/javascript',
+    '.gif'  :   'image/gif',
+    '.jpg'  :   'image/jpeg',
+    '.png'  :   'image/png'
+};
+
+//do all website routing for serving the staic files
 function router(req,res) {
     var baseURI = url.parse(req.url, true);
-    var rootDir = __dirname; 
     var filePath = __dirname + (baseURI.pathname === '/' ? '/index.html' : baseURI.pathname);
     console.log(filePath);
-    var resolveRoute = routes[req.method][baseURI.pathname];
-    if(resolveRoute !== undefined) {
-        req.queryParams = baseURI.query;
-        resolveRoute(req,res);
-    } else {
-        routes.NA(req,res);
-    }
+    fs.access(filePath, fs.F_OK, (error) => {
+        if(!error) {
+            fs.readFile(filePath, (error, content) => {
+                if(!error) {
+                    var contentType = mimes[path.extname(filePath)];
+                    res.writeHead(200, {'Content-type': contentType});
+                    res.end(content, 'utf-8');
+                } else {
+                    res.writeHead(500);
+                    res.end("<h1>File not found</h1>");
+                }
+            });
+        } else {
+            throw error; 
+        }
+    });
 }
 
 
