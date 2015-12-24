625a71ffae671b52025edabf54e070978376b2e6
Udaiveer <uda4000@hotmail.com>
2015-12-23 17:04:21 -0800

    serving index.js from node project with handelbars partials working

--- compiler/routes/index.js
+++ compiler/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/test', function(req, res, next) {
   res.render('test', { title: 'Express' });
 });
 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 module.exports = router;
