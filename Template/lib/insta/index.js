aef0d4bbf34780ebeb2e7ed6066449e1bda88aca
Udaiveer <uda4000@hotmail.com>
2016-01-17 05:21:31 -0800

    hotspots correctly grouping by gps

--- Template/lib/insta/index.js
+++ Template/lib/insta/index.js
@@ -1,6 +1,82 @@
 var request = require('request')
+var mongoose = require('mongoose');
 
-var aUrl = "http://localhost:9000/instaPics";
+function getLatLanIndex(lat, lan, acc) {
+    lat = (lat+"").split(".");
+    lan = (lan+"").split(".");
+    if(!acc) {
+        acc = 2;
+    }
+    var latTmp = lat[0] + "." +  lat[1].substring(0,acc);
+    var lanTmp = lan[0] + "." + lan[1].substring(0,acc);
+    return [latTmp,lanTmp];
+}
+
+var BucketSchema = mongoose.Schema({
+    lat: String,
+    lng : String,
+    images: [{ lowResUrl: String, highResUrl: String, time: Number, tags: []}]
+});
+
+var Bucket = mongoose.model('buckets', BucketSchema);
+
+
+function filterData(data) {
+    var filtered = {};
+    try {
+        data = data.data;
+        data.forEach(function (elem) {
+            var lat = elem.location.latitude;
+            var lan = elem.location.longitude;
+            var indexes = getLatLanIndex(lat, lan, 3);
+            var indexLat = indexes[0];
+            var indexLng = indexes[1];
+            var index = indexLat + "_" + indexLng;
+            if (!filtered[index]) {
+                filtered[index] = {};
+                filtered[index].lat = indexLat;
+                filtered[index].lng = indexLng;
+                var imagesObj = parseImages(elem);
+                filtered[index].images  = [imagesObj];
+            }
+            else {
+                var imagesObj2 = parseImages(elem);
+                filtered[index].images.push(imagesObj2);
+            }
+
+        });
+    } catch (e) {
+        console.log(e.stack);
+    }
+    storeBuckets(filtered);
+    return filtered;
+}
+
+function storeBuckets(buckets) {
+   for(var key in buckets)   {
+       var aBucket = buckets[key];
+       var myBucket = new Bucket(aBucket);
+        myBucket.save(function(err,obj) {
+            if(err) {
+              console.log(err);
+            } else {
+              console.log("obj was saved ");
+              console.log(obj);
+            }
+        });
+   }
+}
+
+function parseImages(elem) {
+    var image = {};
+    if(elem.images) {
+        image.lowResUrl = elem.images.low_resolution.url;
+        image.highResUrl = elem.images.standard_resolution.url;
+        image.time = elem.created_time;
+        image.tags = elem.tags;
+    }
+    return image;
+}
 
 function getCurrentImages(lat, lang, distance, req, res, updateDatabse) {
     var instaToken = "1539375414.31aeb77.fb6ade7acfce42719298f3745c4791be"
@@ -19,31 +95,27 @@ function getCurrentImages(lat, lang, distance, req, res, updateDatabse) {
             gzip: false
         }
         , function (error, response, body) {
-            // body is the decompressed response body
             console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'))
-            try {
-                var json = JSON.parse(body);
-                console.log("got here");
-                //console.log(json);
-                if(updateDatabse) {
-
-                    request({
-                        url: aUrl,
-                        method: "POST",
-                        json: true,
-                        headers: {
-                            "content-type": "application/json",
-                        },
-                        body: json
-                    }, function(err, repsonse, body) {
-                       //console.log(err);
-                        //console.log(response);
-                        //console.log(body);
-                    });
+            if(!updateDatabse) {
+                try {
+                    var json = JSON.parse(body);
+                    res.json(json.data);
+                } catch (e) {
+                    res.status(400).send("instagram could not send data");
                 }
-                res.json(json.data);
-            } catch (e) {
-                res.status(400).send("instagram could not send data");
+            } else {
+                try {
+                    var json = JSON.parse(body);
+                    //console.log(json);
+                    var myData = filterData(json);
+                    console.log(myData);
+                    for(var key in myData) {
+                       console.log(myData[key].images);
+                    }
+                } catch (e)  {
+                    console.log("no updates could be made to DB")
+                }
+
             }
         }
     );
--- Template/routes/index.js
+++ Template/routes/index.js
@@ -2,25 +2,26 @@ var express = require('express'); var router = express.Router();
 var mongoose = require('mongoose');
 var insta = require('../lib/insta');
 
-var resetDb = true;
-
-var BucketSchema = mongoose.Schema({
-    lat: Number,
-    lng : Number,
-    images: [{ url: String, name: String, time: Number , tags: []}]
-});
-
-var Bucket = mongoose.model('buckets', BucketSchema);
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
       res.render('index', { title: 'Express' });
 });
 
+setInterval(function(){
+    console.log('test');
+    var resetDb = true;
+    var lat, lang, distance;
+    lat = 37.787452;
+    lang = -122.396906;
+    distance = 5000;
+    console.log(lat + " " + " " + lang + " "  + distance);
+    insta.getInstaImages(lat,lang, distance, null, null, resetDb);
+}, 5000);
 
 router.get('/newLocation', function(req, res, next) {
     try {
-      var lat, lang, distance;
+    var resetDb = false;
+    var lat, lang, distance;
     lat = 37.787452;
     lang = -122.396906;
     distance = 5000;
@@ -31,112 +32,31 @@ router.get('/newLocation', function(req, res, next) {
     }
 });
 
-router.post('/instaPics', function(req, res, next) {
-    console.log("got data from insta ");
-      var data = req.body;
-      console.log(data);
-      filterData(data);
-      res.status(200).send("ok");
-});
 
-function getLatLanIndex(lat, lan, acc) {
-    lat = (lat+"").split(".");
-    lan = (lan+"").split(".");
-    if(!acc) {
-        acc = 3;
-    }
-    var latTmp = lat[0] + "." +  lat[1].substring(0,acc);
-    var lanTmp = lan[0] + "." + lan[1].substring(0,acc);
-    return [latTmp,lanTmp];
-}
+//
+//function addEntry() {
+//    var myBucket = new Bucket();
+//    var x = Math.random();
+//    myBucket.title = "title" + x;
+//    myBucket.lat = x;
+//    myBucket.lng = x;
+//    myBucket.images = [];
+//    for(var i = 0; i < 4; i++) {
+//        var y = {}
+//        y.url = "aurl/stf/sdfdsfss.com";
+//        y.time =  x;
+//        for(var t =0; t < 4; t++) {
+//            if(!y.tags) {
+//                y.tags = [];
+//            } else {
+//                y.tags.push('tag' + t);
+//            }
+//        }
+//        myBucket.images.push(y);
+//        console.log(y);
+//    }
+//    return myBucket;
+//}
 
-function filterData(data) {
-    try {
-        var filtered = {};
-        data = data.data;
-        console.log("here 1");
-        data.forEach(function (elem) {
-            var lat = elem.location.latitude;
-            var lan = elem.location.longitude;
-            console.log("here 2")
-            var indexes = getLatLanIndex(lat, lan, 4);
-            console.log("here 2")
-            var indexLat = indexes[0];
-            var indexLng = indexes[1];
-            console.log("here 2")
-            var index = indexLat + "_" + indexLng;
-            console.log("here 2")
-            if (!filtered[index]) {
-                filtered[index] = {};
-                filtered[index].lat = indexLat;
-                filtered[index].lng = indexLng;
-                console.log("here 3")
-                var imagesObj = parseImages(elem);
-                console.log("here 3")
-                filtered[index].images  = [imagesObj];
-            }
-            else {
-                var imagesObj2 = parseImages(elem);
-                filtered[index].images.push(imagesObj2);
-            }
-            console.log("here 2")
-        });
-        console.log(filtered);
-        for(key in filtered) {
-           console.log(filtered[key].images);
-        }
-    } catch (e) {
-        console.log(e.stack);
-    }
-}
-
-function parseImages(elem) {
-    var image = {};
-    if(elem.images) {
-        image.lowResUrl = elem.images.low_resolution.url;
-        image.highResUrl = elem.images.standard_resolution.url;
-        image.time = elem.created_time;
-        image.tags = elem.tags;
-    }
-    return image;
-}
-
-
-function addEntry() {
-    var myBucket = new Bucket();
-    var x = Math.random();
-    myBucket.title = "title" + x;
-    myBucket.lat = x;
-    myBucket.lng = x;
-    myBucket.images = [];
-    for(var i = 0; i < 4; i++) {
-        var y = {}
-        y.url = "aurl/stf/sdfdsfss.com";
-        y.time =  x;
-        for(var t =0; t < 4; t++) {
-            if(!y.tags) {
-                y.tags = [];
-            } else {
-                y.tags.push('tag' + t);
-            }
-        }
-        myBucket.images.push(y);
-        console.log(y);
-    }
-    return myBucket;
-}
-
-router.post('/', function(req, res, next){
-    var aBucket = addEntry();
-    aBucket.save(function(err,obj) {
-    if(err) {
-      console.log(err);
-      res.statusCode(500).send();
-    } else {
-      console.log("obj was saved ");
-      res.json(obj);
-    }
-    });
-});
 
 module.exports = router;
