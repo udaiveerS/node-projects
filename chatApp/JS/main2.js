f7afa19d4f3a4471b0549b0feb3630c3192683a8
Udaiveer <uda4000@hotmail.com>
2016-01-21 03:44:57 -0800

    changed var for production

--- chatApp/JS/main2.js
+++ chatApp/JS/main2.js
@@ -7,9 +7,9 @@ var activeUsers = 0;
 // socket.io endpoint
 var suffix = '/socket.io/socket.io.js';
 
-//var servo = 'q-apps.io';
-var servo = '';
-var host = 'localhost';
+var servo = 'q-apps.io';
+//var servo = '';
+//var host = 'localhost';
 //socket up URI
 var ip = 'http://' + (servo||host) + ':9000';
 //var ip = 'http://' + (servo||host);
